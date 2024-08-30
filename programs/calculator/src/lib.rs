5359850a98bd02f945f584a9706ac5f338721bd1
Dipesh Paudel <thedeepyash7@gmail.com>
2024-08-30 12:28:50 +0545

    write and test create and add calculator program

--- programs/calculator/src/lib.rs
+++ programs/calculator/src/lib.rs
@@ -4,13 +4,41 @@ declare_id!("FiDdfRv2y6JVYGZVUw7QqxXhE2zUg5yFXvskwvwTHCdK");
 
 #[program]
 pub mod calculator {
+    use anchor_lang::solana_program::entrypoint::ProgramResult;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create(ctx: Context<Create>, initial_message: String) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.greeting = initial_message;
+        Ok({})
+    }
+
+    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 + num2;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer=user, space=264)]
+    pub calculator: Account<'info, Calculator>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Addition<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+#[account]
+pub struct Calculator {
+    greeting: String,
+    result: i64,
+}
--- tests/calculator.ts
+++ tests/calculator.ts
@@ -1,16 +1,53 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Calculator } from "../target/types/calculator";
+import { expect } from "chai";
+const { SystemProgram } = anchor.web3;
 
 describe("calculator", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
+  // Referencing the program - Abstraction that allows us to call methods of our SOL program.
   const program = anchor.workspace.Calculator as Program<Calculator>;
+  const programProvider = program.provider as anchor.AnchorProvider;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  // Generating a key-pair for our Calculator account
+  const calculatorPair = anchor.web3.Keypair.generate();
+
+  const text = "This is initial message!";
+
+  // TEST BLOCK
+  it("Creation", async () => {
+    // Calling create instance - Set our calculator key-pair as a signer
+    await program.methods
+      .create(text)
+      .accounts({
+        calculator: calculatorPair.publicKey,
+        user: programProvider.wallet.publicKey,
+      })
+      .signers([calculatorPair])
+      .rpc();
+
+    // We fetch that account and read if the string is actually in the account
+    const account = await program.account.calculator.fetch(
+      calculatorPair.publicKey
+    );
+    expect(account.greeting).to.eql(text);
+  });
+
+  // TEST BLOCK
+  it("Addition", async () => {
+    await program.methods
+      .add(new anchor.BN(2), new anchor.BN(3))
+      .accounts({
+        calculator: calculatorPair.publicKey,
+      })
+      .rpc();
+
+    const account = await program.account.calculator.fetch(
+      calculatorPair.publicKey
+    );
+    expect(account.result).to.eql(new anchor.BN(5));
   });
 });
